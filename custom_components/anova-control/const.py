99a7407f446d2ec199e46c427c6710c247d3ef27
Brian Alson <brian@bdalson.com>
2024-12-01 17:36:24 -0500

    Initial commit

---
+++ custom_components/anova-control/const.py
@@ -0,0 +1,6 @@
+"""Constants for the Anova integration."""
+
+DOMAIN = "anova-control"
+
+ANOVA_CLIENT = "anova_api_client"
+ANOVA_FIRMWARE_VERSION = "anova_firmware_version"
--- tests/const.py
+++
@@ -1,9 +0,0 @@
-"""Constants for Cookiecutter Home Assistant Custom Component Instance tests."""
-from custom_components.cc_ha_cci.const import (
-    CONF_PASSWORD,
-)
-from custom_components.cc_ha_cci.const import (
-    CONF_USERNAME,
-)
-
-MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
